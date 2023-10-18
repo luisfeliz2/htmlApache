e24c5d810d3f3582bb6e79e36f10ec2b81b88c80
luisfeliz2 <luisfelizbeltre@gmail.com>
2023-10-18 22:48:48 +0200

    df

---
+++ Ejercicios_DDR/enviardatos.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="" method="post">
+        <h1>Promedio</h1>
+        <label for="">Nombre: </label>
+        <input type="text" name="nombre">
+        <label for="">Nota1: </label>
+        <input type="text" name="nombre">
+        <label for="">Nota2: </label>
+        <input type="text" name="nombre">
+        <input type="submit" value="Calcular" name="submit">
+        
+    </form>
+    
+</body>
+</html>
